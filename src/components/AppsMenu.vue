fa704bdcfdb3c97c84aa0c3c006dae7a9046f0b8
Joan Galmés Riera <joan.galmes@gmail.com>
2023-03-30 16:42:59 +0200

    dockeritzat grupscooperatius app

--- src/components/AppsMenu.vue
+++ src/components/AppsMenu.vue
@@ -5,6 +5,12 @@
         <q-item v-if="appUsuarisEnabled" clickable v-close-popup :href="appUsuarisURL">
           <q-item-section>Usuaris</q-item-section>
         </q-item>
+        <q-item v-if="appConvalidacionsEnabled" clickable v-close-popup :href="appConvalidacionsURL">
+          <q-item-section>Convalidacions</q-item-section>
+        </q-item>
+        <q-item v-if="appWebIesManacorEnabled" clickable v-close-popup :href="appWebIesManacorURL">
+          <q-item-section>Web</q-item-section>
+        </q-item>
       </q-list>
     </q-menu>
   </q-btn>
@@ -20,7 +26,11 @@ export default defineComponent({
   data(){
     return {
       appUsuarisEnabled: process.env.APP_ENABLE_USUARIS,
-      appUsuarisURL: process.env.APP_URL_USUARIS
+      appUsuarisURL: process.env.APP_URL_USUARIS,
+      appConvalidacionsEnabled: process.env.APP_ENABLE_CONVALIDACIONS,
+      appConvalidacionsURL: process.env.APP_URL_CONVALIDACIONS,
+      appWebIesManacorEnabled: process.env.APP_ENABLE_WEBIESMANACOR,
+      appWebIesManacorURL: process.env.APP_URL_WEBIESMANACOR
     }
   },
   methods: {
